f7c08efd23c711d3d222a08404489eafff759f07
Dane Stevens <dane@dsmedia.ca>
2020-11-24 13:02:11 -0500

    update

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -1,24 +1,72 @@
 import React from 'react'
 import { DateTime } from 'luxon'
 
-export default function ({ title, venue, startDate }){
+import './event.scss'
+
+const EVENT_TYPE = {
+    SCHEDULED: 'EventScheduled',
+    MOVED_ONLINE: 'EventMovedOnline',
+    POSTPONED: 'EventPostponed',
+    RESCHEDULED: 'EventRescheduled',
+    CANCELLED: 'EventCancelled'
+}
+
+const EVENT_ATTENDANCE_MODE = {
+    OFFLINE: 'OfflineEventAttendanceMode',
+    ONLINE: 'OnlineEventAttendanceMode',
+    MIXED: 'MixedEventAttendanceMode'
+}
+
+export default function ({ title, venue, description, startDate, endDate, price, status = 'SCHEDULED', attendanceMode = 'OFFLINE' }){
+
+    const data = {
+        "@context": "https://schema.org/",
+        "@type": "Event",
+        name: title,
+        startDate: startDate + '-05:00',
+        ...(endDate && { endDate: endDate + '-05:00' }),
+        description,
+        eventStatus: `https://schema.org/${ EVENT_TYPE[status] }`,
+        eventAttendanceMode: `https://schema.org/${ EVENT_ATTENDANCE_MODE[attendanceMode] }`,
+        location: {
+            "@type": "Place",
+            name: venue.name,
+            address: {
+                "@type": "PostalAddress",
+                streetAddress: venue.address.address,
+                addressLocality: venue.address.city,
+                addressRegion: venue.address.province,
+                postalCode: venue.address.postalCode,
+                addressCountry: 'CA'
+            }
+        },
+        ...(price && {
+            offers: {
+                "@type": 'Offer',
+                price: price,
+                priceCurrency: 'CAD'
+            }
+        })
+        
+    }
+
     return (
         <>
-            <div
-                vocab='https://schema.org/'
-                typeof='Event'
-            >
-                <div className='eventTitle' property='name'>{ title }</div>
-                <div property='startDate' content=''>{ startDate }</div>
-                <div className='eventVenue' property='location' typeof='Place'>
-                    <div property='name'>{ venue.name }</div>
-                    <div property='address' typeof='PostalAddress'>
-                        <div property='streetAddress'>{ venue.address.address }</div>
-                        <div property='addressLocality'>{ venue.address.city }</div>
-                        <div property='addressRegion'>{ venue.address.province }</div>
-                        <div property='postalCode'>{ venue.address.postalCode }</div>
-                    </div>
+            <div className='eventContainer'>
+                <div className='eventCalendar'>
+                    <div className='eventCalendarMonth'>{ DateTime.fromISO(startDate).toFormat('LLL') }</div>
+                    <div className='eventCalendarDay'>{ DateTime.fromISO(startDate).day }</div>
                 </div>
+                <div className='eventTitle'>{ title }</div>
+                <div >{ startDate }</div>
+                {
+                    endDate && <div>{ endDate }</div>
+                }
+                <script type='application/ld+json'
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify(data)
+                    }}
+                />                
             </div>
         </>
     )
--- src/pages/events/the-christmas-winter-festival.js
+++ src/pages/events/the-christmas-winter-festival.js
@@ -24,7 +24,50 @@ const pageData = {
     keywords: 'christmas festival'
 }
 
+const eventData = {
+    title: frontmatter.title,
+    price: 30,
+    description: `Active Lives Day Program & Teen Ranch are going to be running a Christmas Winter Festival from December 1st to December 27th . This 27-day festival will be located at Teen Ranch in Caledon, Ontario. The Christmas Winter Festival is a drive-in event that allows participants to view 30 plus 3D Christmas hologram displays from their cars. These displays range from a sneak peek at elves creating mischief in Santa's Workshop, watching Olaf throwing snowballs and even witnessing Santa practicing sleigh take offs for Christmas. Every festival each year has a display of colourful lights but no Christmas festival has an interactive display such as the Christmas Winter Festival. This year there will be a live nativity scene on weekends thanks to families from Orangeville and virtual Christmas Carolers singing songs directly to your car. We will also have FM transmitters sending sound directly to your car radio and 3-D Christmas projections. This is the ultimate way to share the holidays with your family.`,
+    venue: {
+        name: 'Teen Ranch',
+        address: {
+            address: '20682 Hurontario Street',
+            city: 'Caledon',
+            province: 'ON',
+            postalCode: 'L7K 1X1'
+        }
+    }
+}
+
 export default function ({ data }) {
+
+    const events = []
+    for (let i = 1; i <= 27; i++) {
+
+        if (i !== 25) {
+
+            events.push(
+                <Event
+                    title={ frontmatter.title }
+                    startDate={`2020-12-${ '00'.substring(0, 2 - i.toString().length) + i.toString() }T19:00:00`}
+                    endDate={`2020-12-${ '00'.substring(0, 2 - i.toString().length) + i.toString() }T21:00:00`}
+                    price={30}
+                    description={`Active Lives Day Program & Teen Ranch are going to be running a Christmas Winter Festival from December 1st to December 27th . This 27-day festival will be located at Teen Ranch in Caledon, Ontario. The Christmas Winter Festival is a drive-in event that allows participants to view 30 plus 3D Christmas hologram displays from their cars. These displays range from a sneak peek at elves creating mischief in Santa's Workshop, watching Olaf throwing snowballs and even witnessing Santa practicing sleigh take offs for Christmas. Every festival each year has a display of colourful lights but no Christmas festival has an interactive display such as the Christmas Winter Festival. This year there will be a live nativity scene on weekends thanks to families from Orangeville and virtual Christmas Carolers singing songs directly to your car. We will also have FM transmitters sending sound directly to your car radio and 3-D Christmas projections. This is the ultimate way to share the holidays with your family.`}
+                    venue={{
+                        name: 'Teen Ranch',
+                        address: {
+                            address: '20682 Hurontario Street',
+                            city: 'Caledon',
+                            province: 'ON',
+                            postalCode: 'L7K 1X1'
+                        }
+                    }}
+                />
+            )
+
+        }
+
+    }
 
 	return (
         <>
@@ -40,18 +83,7 @@ export default function ({ data }) {
 			/>
 
             <Container>
-                <Event
-                    name={ frontmatter.title }
-                    venue={{
-                        name: 'Teen Ranch',
-                        address: {
-                            address: '20682 Hurontario Street',
-                            city: 'Caledon',
-                            province: 'ON',
-                            postalCode: 'L7K 1X1'
-                        }
-                    }}
-                />
+                { events }
             </Container>
 
            
